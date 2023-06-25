bb723728fd2265ca80b7cea3b6f6b70137e01cd1
TheQuintillo <97258847+TheQuintillo@users.noreply.github.com>
2023-06-25 19:03:01 +0200

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -2,13 +2,13 @@ import { MongoClient } from 'mongodb';
 
 async function connectToDB() {
   const uri = 'mongodb://localhost:27017';
-  const dbName = 'trainEngine'; // URL de conexión a MongoDB
+  const dbName = 'trainEngine'; 
 
   try {
     const client = new MongoClient(uri);
     await client.connect();
     console.log('Conexión exitosa a MongoDB');
-    return client.db(dbName); // Devuelve la instancia de la base de datos
+    return client.db(dbName); 
   } catch (error) {
     console.error('Error al conectar a MongoDB:', error);
     throw error;
